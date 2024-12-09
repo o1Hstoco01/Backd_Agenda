6586c06a5279e45dace8efe9ebe33d8302478bf9
sophia.caires <henristoco@gmail.com>
2024-12-09 16:06:48 -0300

    criando metodo get_id

--- src/main/java/br/fatec/Agenda/controller/AgendaControler.java
+++ src/main/java/br/fatec/Agenda/controller/AgendaControler.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,10 @@ public class AgendaControler {
     @GetMapping
     public ResponseEntity<List<AgendaResponce>>getAgenda(){
         return ResponseEntity.ok(this.service.getAgenda());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<AgendaResponce> getAgenda(@PathVariable long id){
+        return ResponseEntity.ok(service.getAgendaById(id));
     }
 }
--- src/main/java/br/fatec/Agenda/service/AgendaService.java
+++ src/main/java/br/fatec/Agenda/service/AgendaService.java
@@ -10,6 +10,7 @@ import br.fatec.Agenda.dto.AgendaResponce;
 import br.fatec.Agenda.entities.Agenda;
 import br.fatec.Agenda.mappers.AgendaMapper;
 import br.fatec.Agenda.repositories.AgendaRepository;
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class AgendaService {
@@ -17,10 +18,17 @@ public class AgendaService {
     @Autowired
     private AgendaRepository repository;
 
-    
+
     public List<AgendaResponce> getAgenda(){
         List<Agenda> agendas = repository.findAll();
         return agendas.stream().map(a -> AgendaMapper.toDTO(a)).collect(Collectors.toList());
     }
     
+
+    public AgendaResponce getAgendaById(long id){
+        Agenda agenda = repository.findById(id).orElseThrow(
+            () -> new EntityNotFoundException("Agenda não cadastrada")
+        );
+        return AgendaMapper.toDTO(agenda);
+    }
 }
